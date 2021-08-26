3b8c78cd883b7d89c83a758809870a549dcb93b9
ar-ekt <57006850+ar-ekt@users.noreply.github.com>
2021-08-26 15:31:00 +0430

    added help text

--- pages/help_page.py
+++ pages/help_page.py
@@ -10,7 +10,17 @@ class HelpPage:
         self.screen = screen
         self.initializeImages()
         self.initializeButtons()
-        
+
+        self.lines = ["The goal of the game is that you reach the sum of 15 with 3",
+                      "cards in your hand. The first person to reach this goal in",
+                      "maximum of 30 turns is the winner.",
+                      "",
+                      "Each time at your turn, you are supposed to take one card",
+                      "from the desk.",
+                      "When you have 3 cards in your hand, you should swap one",
+                      "of your cards in your hand with a card on the desk.",
+                      "",
+                      "If you have any questions, feel free to ask your supervisor."]
     
     def initializeButtons(self):
         self.mainPageButton = Button(self.screen, (445, 512, 132, 44), Color.RED, Color.BLACK, Font.make("Garamond", 30), "Main Page", "Main")
@@ -21,6 +31,8 @@ class HelpPage:
     def show(self):
         self.screen.blit(self.mainBackgroundImage, (0, 0))
         self.screen.blit(Font.make("Algerian", 116).render("FIFTEEN", 1, Color.BLACK), (290, 0))
+        for index, line in enumerate(self.lines):
+            self.screen.blit(Font.make("GaramondBold", 28).render(line, 1, Color.BLACK), (160, 140 + index * 34))
         self.mainPageButton.show()
         
         pygame.display.flip()
